0b7fe4b549fded63eee692a26bb2dd3b4e004919
Eray Şen <eraysen@gmail.com>
2021-10-13 20:38:40 +0300

    ..

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {}
-  },
-  plugins: ['@tailwindcss/jit', 'autoprefixer']
+  }
+  //plugins: ['@tailwindcss/jit', 'autoprefixer']
 }
